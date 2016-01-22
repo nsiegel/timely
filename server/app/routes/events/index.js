8875dc7db455b3ad846dd6b5241620f2d2e53288
Nicole <nsiegel2@gmail.com>
2016-01-21 23:27:23 -0500

    route to update event & add error handling

--- server/app/routes/events/index.js
+++ server/app/routes/events/index.js
@@ -7,6 +7,9 @@ router.get('/', function(req, res, next) {
   Event.find({}).exec()
     .then(function(events) {
       res.status(200).json(events);
+    }).then(null, function(err) {
+      err.message = "Something went wrong finding all events!";
+      next(err);
     });
 });
 
@@ -14,5 +17,19 @@ router.post('/', function(req, res, next) {
   Event.create(req.body)
     .then(function(createdEvent) {
       res.status(201).json(createdEvent);
+    }).then(null, function(err) {
+      err.message = "Something went wrong creating an event!";
+      next(err);
+    });
+});
+
+router.put('/:eventId', function(req, res, next) {
+  var id = req.params.eventId;
+  Event.findByIdAndUpdate(id, { $set: req.body })
+    .then(function(updatedEvent) {
+      res.status(200).json(updatedEvent);
+    }).then(null, function(err) {
+      err.message = "Something went wrong updating this event!";
+      next(err);
     });
 });

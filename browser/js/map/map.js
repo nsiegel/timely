4c68dfa1bcd014224838c8e0ccfa7cb8c4efcfe1
Nicole <nsiegel2@gmail.com>
2016-01-22 12:40:50 -0500

    add map

---
+++ browser/js/map/map.js
@@ -0,0 +1,18 @@
+app.config(function($stateProvider) {
+  $stateProvider.state('map', {
+    url: '/map',
+    templateUrl: '/js/map/map.html',
+    controller: 'MapCtrl'
+  })
+});
+
+app.controller('MapCtrl', function($scope) {
+  mapboxgl.accessToken = 'pk.eyJ1IjoibnNpZWdlbDIiLCJhIjoiY2lqcHY1OTBuMDFkMXRvbTV3eTZ6MXdycSJ9.AO_LTuXwMnDabyGfYydArw';
+  var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v8', //stylesheet location
+    center: [-73.99, 40.73], // starting position
+    zoom: 13 // starting zoom
+  });
+
+});

c63e05489e5ae7e2d92ed1899b1c72ce2e9b19bc
Nicole <nsiegel2@gmail.com>
2016-01-21 23:21:23 -0500

    route to get all events

--- server/app/routes/events/index.js
+++ server/app/routes/events/index.js
@@ -1,3 +1,12 @@
 'use strict';
 var router = require('express').Router();
+var Event = mongoose.model('Event');
 module.exports = router;
+
+router.get('/', function(req, res, next) {
+  Event.find({}).exec()
+    .then(function(events) {
+      res.status(200).json(events);
+    });
+});
+

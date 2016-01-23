40728d645b04d00de4a38a1afa44d9d4e95e712a
Nicole <nsiegel2@gmail.com>
2016-01-23 11:16:25 -0500

    preserve, add lat and long based on location string

--- server/db/models/events.js
+++ server/db/models/events.js
@@ -45,4 +45,21 @@ var schema = new mongoose.Schema({
     }
 });
 
+schema.pre('save', function(next) {
+    var event = this;
+    var query = event.endLocation.place.replace(' ', '+');
+    console.log('THIS IS QUERY: ', query);
+    var url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + query + '.json?' +
+        'access_token=pk.eyJ1IjoibnNpZWdlbDIiLCJhIjoiY2lqcHY1OTBuMDFkMXRvbTV3eTZ6MXdycSJ9.AO_LTuXwMnDabyGfYydArw';
+    rp(url)
+        .then(function(res) {
+            var info = JSON.parse(res);
+            event.endLocation.lat = info.features[0].geometry.coordinates[0];
+            event.endLocation.lng = info.features[0].geometry.coordinates[1];
+            console.log(event);
+            next();
+        }).then(null, console.err);
+
+});
+
 mongoose.model('Event', schema);

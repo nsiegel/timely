309f2c090bce2422a843518d429d0ae6924e1438
Nicole <nsiegel2@gmail.com>
2016-01-24 12:19:01 -0500

    update single-event page with map info

---
+++ browser/js/common/factories/geo.factory.js
@@ -0,0 +1,28 @@
+app.factory('GeoFactory', function($window, $q, $http) {
+    return {
+        getGeoLocation: function() {
+            var deferred = $q.defer();
+
+            if (!$window.navigator.geolocation) {
+                deferred.reject('Geolocation not supported.');
+            } else {
+                $window.navigator.geolocation.getCurrentPosition(
+                    function (position) {
+                        deferred.resolve(position);
+                    },
+                    function (err) {
+                        deferred.reject(err);
+                    });
+            }
+
+            return deferred.promise;
+        },
+        getDirectionCoords: function(url) {
+            return $http.get(url)
+            .then(function(res) {
+                console.log(res.data)
+                return res.data;
+            }).then(null, console.err);
+        }
+    };
+});
